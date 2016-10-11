7cdb96665de2ea91472940d8333513166a6ac927
Wesley Robinson <Robinson.WesleyD@gmail.com>
2016-10-11 03:47:34 -0700

    implement with tachyons

--- public/app.js
+++ public/app.js
@@ -1,58 +1,61 @@
-// logic
+$(document).ready(function () {
 
-// app
-function roll(sides) {
-    return Number.isInteger(sides) ? (Math.floor(Math.random() * sides)) + 1 : new TypeError('sides must be an integer');
-}
-function handleDice(e) {
-    var validDice = ['d3', 'd4', 'd6', 'd10', 'd100'];
-    var id = e.target.id;
-    if (!validDice.includes(id)) return;
-
-    e.preventDefault();
-    var diceResult = document.querySelector('#dice-result');
-    var sides = parseInt(id.match(/\d+/)[0]);
-    var rollResult = roll(sides);
-
-    if (sides === 100) {
-        var rollResult2 = roll(sides);
-        console.log(rollResult, rollResult2);
-        var bonus = document.querySelector('#bonus-die').checked ? Math.min(rollResult, rollResult2) : false;
-        var penalty = document.querySelector('#penalty-die').checked ? Math.max(rollResult, rollResult2) : false;
+    // logic
+    function roll(sides) {
+        return Number.isInteger(sides) ? (Math.floor(Math.random() * sides)) + 1 : new TypeError('sides must be an integer');
     }
 
-    var double = document.querySelector('#double').checked;
-    var triple = document.querySelector('#triple').checked;
-    if (double) rollResult *= 2;
-    if (triple) rollResult *= 3;
+    // app event handlers
+    function handleDice(e) {
+        var validDice = ['d3', 'd4', 'd6', 'd10', 'd100'];
+        var id = e.target ? e.target.id : null;
+        if (!validDice.includes(id)) return e;
+        e.preventDefault();
 
-    var addMode = document.querySelector('#add-mode').checked;
-    var sumNums = document.querySelector('#sum-nums');
-    sumNums.value = addMode ? sumNums.value += rollResult : null;
-    sumNums.innerHTML = sumNums.value || null;
+        var $diceResult = document.querySelector('#dice-result');
+        var sides = parseInt(id.match(/\d+/)[0]);
+        var rollResult = roll(sides);
 
-    if (penalty) { diceResult.innerHTML = penalty }
-    else if (bonus) { diceResult.innerHTML = bonus }
-    else diceResult.innerHTML = rollResult;
+        if (sides === 100) {
+            var rollResult2 = roll(sides);
+            console.log(rollResult, rollResult2);
+            var bonus = $('#bonus').hasClass('is-active') ? Math.min(rollResult, rollResult2) : false;
+            var penalty = $('#penalty').hasClass('is-active') ? Math.max(rollResult, rollResult2) : false;
+        }
 
-    return e;
-}
-document.querySelector('#dice-box').addEventListener('click', handleDice, false);
+        var double = $('#double').hasClass('is-active');
+        var triple = $('#triple').hasClass('is-active');
+        if (double) rollResult *= 2;
+        if (triple) rollResult *= 3;
 
-// API
-var die = parseInt(document.URL.split('#')[1]);
-if (Number.isInteger(die)) {
-    JSON.stringify({
-        status: 200,
-        value: roll(die)
-    });
-}
+        var addMode = $('#add-mode').hasClass('is-active');
+        var sumNums = document.querySelector('#sum-nums');
+        sumNums.value = 0;
+        sumNums.value = addMode ? sumNums.value += rollResult : null;
+        sumNums.innerHTML = sumNums.value;
 
-//404.js
+        if (penalty) {$diceResult.innerHTML = penalty}
+        else if (bonus) {$diceResult.innerHTML = bonus}
+        else $diceResult.innerHTML = rollResult;
 
-// helpers
-function toggleDiv(divId) {
-    var div = document.querySelector('#' + divId);
-    div.hidden = !div.hidden;
-    return div;
-}
+        return e;
+    }
+
+    function toggleActive(e) {
+        var $t = $(e.target);
+        $t.toggleClass('is-active');
+        $t.attr('class').split(' ').forEach(grabColor);
+        function grabColor(el){
+            if (el.slice(0, 6) === 'hover-') {
+                $t.toggleClass('bg-' + el.slice(6));
+            }
+        }
+    }
+    var diceModifier = $('.dice-modifier');
+    diceModifier.click(toggleActive);
+
+    var diceBox = $('#dice-box');
+    diceBox.click(handleDice);
+
+    // helpers
+});
